4baf4136d74cf17f2f59a76eeaccb30b1955c9d9
Jessica-Lee1424 <jgonnella14@gmail.com>
2024-09-20 14:02:13 -0400

    read me additions

--- README.md
+++ README.md
@@ -1,42 +1,20 @@
-# README_Generator
-Module 7 Assignment Repository
 
-For our Module 7 assignment, I wanted to provide a thorough document of how to create a proper README.md, including a README.md generator that allows the user to quickly create a professional README.md when beginning a new project. Doing this will allow the user to set up their project more quickly and efficiently. In doing so, I have learned how to install package.JSON packages....
-
-## Table of Contents (Optional)
-
-If your README is long, add a table of contents to make it easy for users to find what they need.
-
-- [Installation](#installation)
-- [Usage](#usage)
-- [Credits](#credits)
-- [License](#license)
-
-## Installation
-
-- Create your repository with desired .gitignore files added
-- Navigate to repository in terminal
-- install npm and package.JSON
-......
-
-## Usage
-
-Provide instructions and examples for use. Include screenshots as needed.
-
-To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
-
-    ```md
-    ![alt text](assets/images/files-screenshot.png)
-    ```
-
-## Credits
-
-List your collaborators, if any, with links to their GitHub profiles.
-
-If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-
-If you followed tutorials, include links to those here as well.
-
-## License
-
-MIT License
+    # Title
+    # Description
+    Description
+    # Installation
+    Install app
+    # Usage
+    For fun
+    # Contributions
+    No one
+    # Testing
+    No tests
+    # GitHub UserName
+    Github
+    # Email Address
+    email
+    # License
+    MIT
+    [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]
+    
--- index.js
+++ index.js
@@ -1,25 +1,91 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
+import inquirer from 'inquirer';
+import fs from 'fs';
 
-inquirer.createPromptModule([
+inquirer.prompt([
     {
         type: 'input',
         name: 'title',
         message: 'What is the title of your project?',
-        email: 'What is your email address?',
     },
     {
         type: 'input',
         name: 'description',
         message: 'Describe your project:',
-        email: 'email address'
+
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How are you installing your application?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How will you be using this application?',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Who else contributed to this application?',
+    },
+    {
+        type: 'input',
+        name: 'testing',
+        message: 'How do you run your tests on this application, if any?',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your e-mail address?',
+    },
+    {
+        type: 'list',
+        name: 'licenseOptions',
+        choices: ['MIT', 'Apache', 'BSD 2-Clause License', 'Boost Software License 1.0'],
+        message: 'How do you run your tests on this application, if any?',
     },
 ]).then(answers => {
+console.log(answers);  
+
     const readmeContent = `
     # ${answers.title}
-    ## Description
+    # Description
     ${answers.description}
+    # Installation
+    ${answers.installation}
+    # Usage
+    ${answers.usage}
+    # Contributions
+    ${answers.contributing}
+    # Testing
+    ${answers.testing}
+    # GitHub UserName
+    ${answers.github}
+    # Email Address
+    ${answers.email}
+    # License
+    ${answers.licenseOptions}
     `;
     fs.writeFileSync('README.md', readmeContent);
     console.log('README.md has been generated!');
 });
+
+
+
+// WHEN I enter my project title
+// THEN this is displayed as the title of the README
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
